96d237932a4e54c77c85859c00a17bb72a2929de
AmirMahdi80 <m.ikani1380@gmail.com>
2021-02-04 00:48:21 +0330

    finished

---
+++ New folder/CarTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void CarTest()
+        {
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            User user = new User("Ali", "mahdavi", "5321234");
+            Car car = new Car("Pride","34M123456","20",user);
+            Assert.AreEqual(car.IsValid(),true);
+        }
+
+        [TestMethod()]
+        public void IsNativeTest()
+        {
+            User user = new User("Ali", "mahdavi", "5321234");
+            Car car = new Car("Pride", "34M123456", "20", user);
+            Assert.AreEqual(car.IsNative(), false);
+            
+        }
+        [TestMethod()]
+        public void IsNativeTest2()
+        {
+            User user = new User("Amir", "mahdavi", "5321236");
+            Car car = new Car("Pride", "34M123433", "20", user);
+            Assert.AreEqual(car.IsNative(), true);
+
+        }
+
+        [TestMethod()]
+        public void IsInTest()
+        {
+            User user = new User("Ali", "mahdavi", "5321234");
+            Car car = new Car("Pride", "34M123456", "20", user);
+            Assert.AreEqual(car.IsIn(), true);
+          
+        }
+        public void IsInTest2()
+        {
+            User user = new User("Amir", "mahdavi", "5321236");
+            Car car = new Car("Pride", "34M123433", "20", user);
+            Assert.AreEqual(car.IsIn(), true);
+
+        }
+
+        [TestMethod()]
+        public void IsValidTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ New folder/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Excel= Microsoft.Office.Interop.Excel;
+//using Microsoft.Office.Interop.Excel;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void T1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string information = Info.Text;
+            try
+            {
+                string[] InfoArr = information.Split('\n');
+                Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+                if (xlApp == null)
+                {
+                    MessageBox.Show("Excel is not properly installed!!");
+                    return;
+                }
+
+                Excel.Workbook xlWorkBook;
+                Excel.Worksheet xlWorkSheet;
+                object misValue = System.Reflection.Missing.Value;
+
+                xlWorkBook = xlApp.Workbooks.Add(misValue);
+                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+
+
+                string NameOfCar = InfoArr[0];
+                string[] InfoOfUser = InfoArr[1].Split(' ');
+                string NameOfUser = InfoOfUser[0];
+                string LNameOfUser = InfoOfUser[1];
+                string IdCode = InfoOfUser[2];
+                
+                string CarCode = InfoArr[2];
+                bool B1= int.TryParse(CarCode.Substring(0, 2), out int n);
+                bool B2= int.TryParse(CarCode.Substring(3), out int m);
+                bool B3 = char.IsUpper(CarCode[2]);
+
+                if(!B1 || !B2 || !B3)
+                {
+                    Console.WriteLine("the problem");
+                    throw new Exception("Your PelakNumber is invalid");
+                }
+                string ProdectDate = InfoOfUser[3];
+                string[] Passengers = InfoOfUser[4].Split(' ');
+                string SeatNumber = Passengers[0];
+                bool B4 = int.TryParse(SeatNumber, out int NP);
+                if (!B4)
+                    throw new Exception("Invalid Number of Passengers");
+
+                string[] AgeOfPassengers = new string[NP];
+                try
+                {
+                    for (int i = 0; i < NP; i++)
+                    {
+                        AgeOfPassengers[i] = Passengers[i + 1];
+
+                    }
+                }
+                catch { }
+
+
+                xlWorkBook.SaveAs("d:\\csharp-Excel.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                xlWorkBook.Close(true, misValue, misValue);
+                xlApp.Quit();
+
+                MessageBox.Show("Saved");
+            }
+
+            catch {
+                MessageBox.Show(":)))"); 
+            }
+            
+        }
+
+        private void Info_Copy_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
